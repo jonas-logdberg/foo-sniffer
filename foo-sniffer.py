aecbf00cf8fc32ca0890f07be0766034c454acee
yip6nine <jonas.logdberg@gmail.com>
2020-05-05 20:36:34 +0200

    Added important stuff on my2ndbranch

--- foo-sniffer.py
+++ foo-sniffer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print("This is the main foo-sniffer python file."
+print("This is the main foo-sniffer python file.")
 print("Adding the 2nd part of the program")
+print("Adding stuff on my2ndbranch")
