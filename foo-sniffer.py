f38649dfa3fd4ac608cd575a736de51a85c8b3ef
yip6nine <jonas.logdberg@gmail.com>
2021-02-16 18:52:45 +0100

    Added VS Code stuff.

--- foo-sniffer.py
+++ foo-sniffer.py
@@ -2,3 +2,6 @@
 print("This is the main foo-sniffer python file.")
 print("Adding the 2nd part of the program")
 print("Adding stuff on my2ndbranch")
+
+print("Adding something in VS Code to test out the integration w GitHub")
+
